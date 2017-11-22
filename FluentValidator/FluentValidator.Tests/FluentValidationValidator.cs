81b83ba57fb04415308830805a35b5bba156e981
Sergey Kichuk <skichuk@luxoft.com>
2017-11-22 19:44:53 +0300

    removed commented code

--- FluentValidator/FluentValidator.Tests/FluentValidationValidator.cs
+++ FluentValidator/FluentValidator.Tests/FluentValidationValidator.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Text.RegularExpressions;
 using FluentValidation;
 
 namespace FluentValidator.Tests
@@ -8,11 +7,6 @@ namespace FluentValidator.Tests
     {
         public bool Configure()
         {
-            //RuleFor(x => x.EmployeeID).GreaterThan(3).WithName("First").WithMessage("Bla bla");
-            //RuleFor(x => x.FirstName).NotEmpty().WithName("First").WithMessage("Bla bla").Matches(new Regex(@"\W")).WithMessage("Not match regex");
-            //RuleFor(x => x.DateOfBirth).LessThan(DateTime.Today).NotNull();
-            //RuleFor(x => x.DateOfBirth).NotNull();
-
             RuleFor(x => x.EmployeeID).GreaterThan(3).WithMessage("Message");
             RuleFor(x => x.FirstName).NotEmpty();
             RuleFor(x => x.DateOfBirth).LessThan(DateTime.Today).NotNull();
